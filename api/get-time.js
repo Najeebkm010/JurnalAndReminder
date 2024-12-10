7440184f5d658b0f955b5853dcf73cdde90583db
Najeebkm010 <140137927+Najeebkm010@users.noreply.github.com>
2024-12-10 15:59:06 +0400

    Update get-time.js

--- api/get-time.js
+++ api/get-time.js
@@ -1,5 +1,9 @@
 export default function handler(req, res) {
   const now = new Date();
+  
+  // Log the server time to the console
+  console.log("Server Time:", now.toISOString());
+
   res.status(200).json({ 
     serverTime: now.toISOString() 
   });
